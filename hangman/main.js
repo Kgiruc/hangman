e05476f2c653fea7a73d5706fe953a02d4a420c6
Karol Giruc <kgiruc76@gmail.com>
2023-01-30 13:23:24 +0100

    some progress

--- hangman/main.js
+++ hangman/main.js
@@ -1,23 +1,34 @@
-const letters = document.getElementById("word");
+const letters = document.getElementById("wordInput");
+const final = document.getElementById("final");
 
 const words = ["lalka", "szczurek", "pirania", "szymon"]
 const word = words[Math.floor(Math.random() * words.length)];
 
-function xyz() {
+const correct = ["i", "l", "s", "z", "y", "m", "o", "n"]
+const wrong = []
+
+function wordResult() {
     letters.innerHTML = `
         ${word
         .split('')
         .map(
-            e => `
+            letter => `
             <span class="border-b-4 border-white">
-            ${e}
+            ${correct.includes(letter) ? letter : " "}
             </span>`
         )
         .join("")
     }`
+
+    const innerWord = letters.innerText.replace(/\n/g, "")
+
+    if(innerWord === word){
+        final.innerText = "brawo kurwa"
+
+    }
 }
 
-xyz()
+wordResult()
 
 
 
